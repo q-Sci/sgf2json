f77dbf3f1d575658bae3960ae24810e2e91fd833
q-Sci <simon.hubert2904@gmail.com>
2023-05-17 16:55:13 +0200

    read sgf content

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use glob::glob;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // create list of all files inside the folder
+    let mut sgf_files: Vec<String> = Vec::new();
+    for file in glob("./src/sgf/*.sgf").expect("Failed to read glob pattern") {
+        sgf_files.push(file.unwrap().display().to_string());
+    }
+
+    // print the content of each sgf file
+    for file in sgf_files {
+        let file_contents =
+            fs::read_to_string(file).expect("Failed to read file");
+        println!("-----\n{file_contents}\n-----\n");
+    }
 }
