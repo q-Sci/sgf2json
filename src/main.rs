7f70e8c88b9815f71dc7cb70decfeda135c4518c
q-Sci <simon.hubert2904@gmail.com>
2023-05-28 19:07:00 +0200

    starting the conversion to the correct syntax

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,34 @@ fn main() {
         sgf_files.push(file.unwrap().display().to_string());
     }
 
-    let mut output_data: Vec<String> = Vec::new(); // content written to json file
+    let mut output_data: Vec<String> = Vec::new(); // content written to json file, not formatted yet
 
     // print the content of each sgf file
     for file in sgf_files {
-        let file_contents =
-            fs::read_to_string(file).expect("Failed to read file");
+        let file_contents = fs::read_to_string(file).expect("Failed to read file");
         output_data.push(file_contents.to_string());
     }
 
-    fs::write("./src/output/problems.json", output_data.join("\n")).expect("Unable to write file");
+    let mut formatted_output_data: Vec<String> = Vec::new(); // formatted content for the json file (correct syntax)
+
+    // formatting the content to json format
+    formatted_output_data.push(concat!("{\n", r#"  "problems": ["#).to_owned()); // first line of json
+    for problem in &output_data {
+        let index = &output_data.iter().position(|r| r == problem).unwrap() + 1; // id of the problem
+
+        let mut obj = concat!("    {\n", r#"      "id": "#).to_string();
+        obj.push_str(&index.to_string());
+        obj.push_str(",\n");
+        obj.push_str(r#"      "problem": "#);
+        obj.push_str(&problem);
+        obj.push_str("\n    },");
+        formatted_output_data.push(obj.to_owned());
+    }
+    formatted_output_data.push("  ],\n}".to_owned()); // last line of json
+
+    fs::write(
+        "./src/output/problems.json",
+        formatted_output_data.join("\n"),
+    )
+    .expect("Unable to write file");
 }
